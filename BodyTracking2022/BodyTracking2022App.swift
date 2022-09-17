b64ac357b6e053c44fec961e1d6c88983d6d5fdf
GKareem <kareem.ghadban@gmail.com>
2022-09-17 19:02:55 -0400

    First Commit

---
+++ BodyTracking2022/BodyTracking2022App.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTracking2022App.swift
+//  BodyTracking2022
+//
+//  Created by Kareem Ghadban on 2022-09-17.
+//
+
+import SwiftUI
+
+@main
+struct BodyTracking2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BodyTracking2022/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BodyTracking2022
+//
+//  Created by Kareem Ghadban on 2022-09-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
