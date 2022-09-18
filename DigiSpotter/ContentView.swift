02bcfe9ff37af61b7d5f2f5b39124c510d1844e1
GKareem <kareem.ghadban@gmail.com>
2022-09-18 01:47:33 -0400

    Initial Commit

---
+++ DigiSpotter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DigiSpotter
+//
+//  Created by Kareem Ghadban on 2022-09-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DigiSpotter/DigiSpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiSpotterApp.swift
+//  DigiSpotter
+//
+//  Created by Kareem Ghadban on 2022-09-18.
+//
+
+import SwiftUI
+
+@main
+struct DigiSpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
